33de6747d7abf0c65f70626aec8ab2f2f83158bc
Xiyang Chen <settinghead@gmail.com>
2014-02-15 22:56:38 -0500

    fixed syntax error in feed detector

--- app/packages/feedcrawler/feedDetector.js
+++ app/packages/feedcrawler/feedDetector.js
@@ -3,7 +3,7 @@ var jobs = Npm.require('kue').createQueue();
 Meteor.methods({
   'feed/queueSource' : function(sourceId) {
     jobs.create('feed', {
-      sourceId: sourceId;
+      sourceId: sourceId
     });
   }
 });
