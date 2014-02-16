4443f2f0c4505ce5cc07c95210870aea5cbb534c
Xiyang Chen <settinghead@gmail.com>
2014-02-15 19:21:07 -0500

    added delete button

--- app/client/views/sources/sources_list.js
+++ app/client/views/sources/sources_list.js
@@ -3,3 +3,10 @@ Template.sources_list.helpers({
     return this.sourcesList;
   }
 });
+
+Template.sources_list.events({
+  'click .delete' : function(e) {
+    var id = $(e.target).data('id');
+    Sources.remove({_id: id});
+  }
+});
