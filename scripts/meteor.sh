8fbc13d2760ba5edfee37eed120db53ecb717d5d
Xiyang Chen <settinghead@gmail.com>
2014-02-27 23:00:15 -0500

    install latest mongodb in deployment script

--- scripts/meteor.sh
+++ scripts/meteor.sh
@@ -32,7 +32,11 @@ apt-get update
 apt-get install -y python-software-properties
 add-apt-repository ppa:chris-lea/node.js
 apt-get update
-apt-get install -y build-essential nodejs mongodb imagemagick libmagick++-dev git graphicsmagick redis-server
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
+sudo apt-get update
+sudo apt-get install mongodb-10gen
+apt-get install -y build-essential nodejs imagemagick libmagick++-dev git graphicsmagick redis-server
 nginx=stable # use nginx=development for latest development version
 add-apt-repository -y ppa:nginx/$nginx
 apt-get update 
