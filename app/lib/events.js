02e471e6fb7e8b9542c728008d4ce698e1b27d00
Xiyang Chen <settinghead@gmail.com>
2014-02-27 22:59:53 -0500

    disable mixpanel

--- app/lib/events.js
+++ app/lib/events.js
@@ -4,9 +4,9 @@ trackEvent = function(event, properties){
   //TODO
   // add event to an Events collection for logging and buffering purposes
   if(Meteor.isClient){
-    if(typeof mixpanel.track !== 'undefined'){
-      mixpanel.track(event, properties);
-    }
+    // if(typeof mixpanel.track !== 'undefined'){
+    //   mixpanel.track(event, properties);
+    // }
     if(typeof GoSquared !== 'undefined' && typeof GoSquared.DefaultTracker !== 'undefined'){
       GoSquared.DefaultTracker.TrackEvent(event, JSON.stringify(properties));
     }
